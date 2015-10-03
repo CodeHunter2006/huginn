cb333806825eb5b7c184d89379d2fff899a3ba0d
CodeHunter2006 <CodeHunter2006@gmail.com>
2015-10-03 10:16:29 +0800

    删除配置文件。

--- config/unicorn.rb
+++
@@ -1,8 +0,0 @@
-working_directory "/home/chengzhuo/huginn"
-pid "/home/chengzhuo/huginn/tmp/pids/unicorn.pid"
-stderr_path "/home/chengzhuo/huginn/log/unicorn_err.log"
-stdout_path "/home/chengzhuo/huginn/log/unicorn_out.log"
-
-listen "/tmp/unicorn.huginn.sock"
-worker_processes 2
-timeout 30
